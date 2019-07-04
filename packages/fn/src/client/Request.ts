53694cc8766fb34909a8a9d1e35c764f28af7164
organic-scholar <nauman1225@gmail.com>
2019-07-04 21:25:04 +0500

    fix: response

--- packages/fn/src/client/Request.ts
+++ packages/fn/src/client/Request.ts
@@ -1,5 +1,14 @@
 import { ISrvRequestParam, IResponseResult } from '../common/interfaces';
 
+export class InvalidResponseException extends Error
+{
+    type = 'InvalidResponse';
+
+    constructor(protected data:object)
+    {
+        super('unable to parse response');
+    }
+}
 
 export class NetworkError extends Error {
 
@@ -25,7 +34,7 @@ export class ServiceRequest
     invoke<T>(param:ISrvRequestParam, config:IRequestConfig):Promise<T>
     {
         let {url, interceptors, ...rest} = config
-        rest  = {...rest, body: JSON.stringify({fn: param})}
+        rest  = {...rest, body: JSON.stringify({fn: param}), method: 'POST'}
         return fetch(url, rest).then((res)=>
         {
             return res.json() as IResponseResult<T>;
@@ -34,14 +43,14 @@ export class ServiceRequest
         {
             if(result.data) return result.data;
             if(result.error) throw Object.assign(new Error(), result.error);
-            throw new Error();
+            throw new InvalidResponseException(result);
         }).catch((err)=>
         {
-            if(err.name === 'TypeError' && err.mesasge === 'Failed to fetch')
+            if(err.name === 'TypeError' && err.message === 'Failed to fetch')
             {
-
+                return Promise.reject(new NetworkError());
             }
-            return Promise.reject(new NetworkError());
+            return Promise.reject(err);
         });
     }
 }
@@ -52,11 +61,20 @@ export class BatchServiceRequest
     {
         if(params.length == 0) return Promise.resolve([]);
         let {url, interceptors, ...rest} = config
-        rest  = {...rest, body: JSON.stringify({fn: params})}
+        rest  = {...rest, body: JSON.stringify({fn: params}), method: 'POST'}
         return fetch(url, rest).then((res)=> res.json())
-        .catch(()=>
+        .then((result)=>
         {
-            return Promise.reject(new NetworkError());
+            if(Array.isArray(result)) return result;
+            throw new InvalidResponseException(result);
+        })
+        .catch((err)=>
+        {
+            if(err.name === 'TypeError' && err.message === 'Failed to fetch')
+            {
+                return Promise.reject(new NetworkError());
+            }
+            return Promise.reject(err);
         });
     }
 }
